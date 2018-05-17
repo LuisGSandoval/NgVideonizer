ff735a9700583a9e5049127c9b9112fa6b88445e
LuisGSandoval <luis.sandoval.457@outlook.com>
2018-05-17 13:41:11 -0500

    se cambian unos nombres

--- src/app/components/video/video.component.ts
+++ src/app/components/video/video.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var M:any;
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.css']
 })
 export class VideoComponent implements OnInit {
-
+  videoPrice : number = 0.03;
   constructor() { }
 
   ngOnInit() {
+    var ModalOptions = new Object();
+    var modalElems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(modalElems, ModalOptions);
+  }
+  Modaltrigger(){
+
   }
 
 }
